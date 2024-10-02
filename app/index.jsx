e8821fec5f7e31cebb0016ee4153593860b89f3d
georgefrg <georgeftw00@gmail.com>
2024-10-02 22:34:59 +0300

    implement search input

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,18 @@
 import { Image, ScrollView, Text, View } from "react-native";
-import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Redirect, router } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { images } from "../constants";
-import CustomButton from "../components/CustomButton";
 import { StatusBar } from "expo-status-bar";
 
+import { images } from "../constants";
+import { useGlobalContext } from "../context/GlobalProvider";
+import CustomButton from "../components/CustomButton";
+
 const App = () => {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if (!isLoading && isLoggedIn) return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
